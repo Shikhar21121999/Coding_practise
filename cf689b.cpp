5be9208dc844afdaf122b6791eaf9de00a64a0c2
shikhar <shikhar21121999@gmail.com>
2020-12-14 01:27:09 +0530

    todays work

--- cf689b.cpp
+++ cf689b.cpp
@@ -95,9 +95,68 @@ using namespace std;
 
 void solve()
 {
-    int n;
-    cin >> n;
-    cout << n << nextline;
+    // initialize two matrices matr,dp
+    // matr stores the matrix values
+    // dp stores the max hieght spruce tree that can be formed with head at the given cell
+
+    // all the cells that have * will be head of the spurce tree of hieght >=1
+    // by same logic all the cells that have . will have spruce tree of hieght 0
+
+    // cells of the last row can only have a spruce tree of max hieght >=1
+
+    int r, c;
+    cin >> r >> c;
+    vector<vector<int>> matr(r, (vector<int>(c, 0)));
+    vector<vector<int>> dp(r, (vector<int>(c, 0)));
+
+    // input values for matr and if matr[i][j]=='*'
+    // initial values for dp[i][j]=1
+
+    for (int i = 0; i <= r; i++)
+    {
+        for (int j = 0; j <= c; j++)
+        {
+            cin >> matr[i][j];
+            if (matr[i][j] == '*')
+                dp[i][j] = 1;
+        }
+    }
+
+    // now we traverse the matr from second last row to top row
+    // and from left to right in column and update the dp[i][j] which corrospond
+    // to max hieght of tree possible at i,j
+
+    for (int i = r - 2; i >= 0; i--)
+    {
+        for (int j = 0; j < c; j++)
+        {
+
+            // if there is no '*' at this cell it implies that spruce tree cannot be found at this cell
+            if (matr[i][j] == '.')
+                continue;
+
+            // else we need to update the hieght of the cell if a spruce tree of greater hieght can be constructed
+            if (j - 1 >= 0 and j + 1 < c and matr[i][j + 1] == '*' and matr[i][j - 1] == '*')
+            {
+                dp[i][j] = dp[i + 1][j] + 1;
+            }
+        }
+    }
+
+    // ans is the sum of all max possbile hieght of the tree in matrix
+    int ans = 0;
+
+    // print the dp array
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            ans += dp[i][j];
+            cout << dp[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << ans << endl;
 }
 
 int main()
@@ -108,6 +167,7 @@ int main()
     freopen("input.txt", "r", stdin);
     // freopen("output.txt","w",stdout);
 #endif
+    cout << "hello" << endl;
     test
     {
         solve();
