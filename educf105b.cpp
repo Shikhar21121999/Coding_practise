b0f8e7c926a803dfc797d50741425454130d8af3
shikharsun <shikhar21121999@gmail.com>
2021-03-05 23:08:10 +0530

    upsolve b of edu

--- educf105b.cpp
+++ educf105b.cpp
@@ -11,49 +11,58 @@ bool solve(){
 	int n,U,R,D,L;
 	cin>>n>>U>>R>>D>>L;
 	
-	// base case 0
-	if(U==0 and R==0 and L==0 and D==0)return true;
-	
-	// base case 1
-	if(U<=n-2 and R<=n-2 and L<=n-2 and D<=n-2)return true;
-	
-	// base case 2
-	if(U==n and R==n and L==n and D==n)return true;
-	
-	// up down rejection 1
-	if(U==n and D==n){
-		if(L<2 or R<2)return false;
+	// create a copy of the above row and columns
+	int U1,R1,D1,L1;
+	U1=U;R1=R;D1=D;L1=L;
+
+	// if equals to n-2 then we paint both row and columns whose corner coincide
+	if(U==(n)){
+		R1--;
+		L1--;
 	}
-	
-	// up down rejection 2
-	if(U==n-1 and D==n-1){
-		if(l)
-	
-	// up rejection 1
-	if(U==n and (R<1 or L<1))return false;
-	
-	// up rejection 2
-	if(U==n-1 and (R<1 and L<1))return false;
-	
-	// left rejection 1
-	if(L==n and (U<1 or D<1))return false;
-	
-	// left rejection 2
-	if(L==n-1 and (U<1 and D<1))return false;
-	
-	// Down rejection 1
-	if(D==n and (R<1 or L<1))return false;
-	
-	// Down rejection 2
-	if(D==n-1 and (R<1 and L<1))return false;
-	
-	// Right rejection 2
-	if(R==n-1 and (R<1 and L<1))return false;
-	
-	// Right rejection 1
-	if(R==n and (U<1 or D<1))return false;
-	
-	return true;
+	if(D==(n)){
+		R1--;
+		L1--;
+	}
+	if(L==(n)){
+		U1--;
+		D1--;
+	}
+	if(R==(n)){
+		U1--;
+		D1--;
+	}
+
+	// if equals to n-1 we paint the row or column which has more cells to paint
+	if(U==(n-1)){
+		if(R1>=L1){
+			R1--;
+		}
+		else L1--;
+	}
+	if(D==(n-1)){
+		if(R1>=L1){
+			R1--;
+		}
+		else L1--;
+	}
+	if(L==(n-1)){
+		if(U1>=D1){
+			U1--;
+		}
+		else D1--;
+	}
+	if(R==(n-1)){
+		if(U1>=D1){
+			U1--;
+		}
+		else D1--;
+	}
+	// cout<<U1<<" "<<R1<<" "<<D1<<" "<<L1<<endl;
+	if(U1>=0 && D1>=0 && L1>=0 && R1>=0){
+		return true;
+	}
+	return false;
 }
 	
 	
